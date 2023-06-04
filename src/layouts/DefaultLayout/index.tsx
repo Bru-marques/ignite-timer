e6d712651083dd23f56c233e88601ce7be29e30b
bru-marques <brmarques1108@gmail.com>
2023-06-04 20:27:51 +0100

    Adding historic page

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -4,10 +4,10 @@ import { Header } from '../../components/Header'
 import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
-    return (
-        <LayoutContainer>
-            <Header />
-            <Outlet />
-        </LayoutContainer>
-    )
+  return (
+    <LayoutContainer>
+      <Header />
+      <Outlet />
+    </LayoutContainer>
+  )
 }
